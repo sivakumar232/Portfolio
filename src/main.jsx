44c65dccb44d49dd7d9bc85b8d5b48ca885e1f05
Sivakumar <sivakumarr3105@gmail.com>
2025-07-24 22:20:31 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import About from './components/About.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
